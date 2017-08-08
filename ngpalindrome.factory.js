93965da98edbc062d4a9fde0fd27b723a842493e
Timothy Batty <tbatty7@gmail.com>
2017-08-08 19:47:11 -0400

    Added isPalindrome function to ngPalindrome factory

--- ngpalindrome.factory.js
+++ ngpalindrome.factory.js
@@ -1,5 +1,13 @@
 var app = angular.module('myModule');
 
-app.factory('PalindromeFactory', function(){
-	return 
-})
+app.factory('PalindromeFactory', function($scope){
+	return {
+		isPalindrome: isPalindrome
+	};
+
+	function isPalindrome(pal){
+		var lowerPal = pal.toLowerCase().replace(/\W/g, '');
+		var palReverse = lowerPal.split('').reverse().join("");
+		return palReverse === lowerPal;
+	}
+});
