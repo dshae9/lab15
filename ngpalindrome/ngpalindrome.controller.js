6e31c76bc0b444d0c220d16f5b62bbca393bef4d
Demonshae Glover <dshaeglover@gmail.com>
2017-08-09 19:19:08 -0400

    made change ngpalindrome.js

--- ngpalindrome/ngpalindrome.controller.js
+++ ngpalindrome/ngpalindrome.controller.js
@@ -6,12 +6,12 @@ app.controller('NgPalindrome', function($scope, PalindromeFactory){
 
   $scope.submit = function(text) {
     if(PalindromeFactory.isPalindrome(text)){
-      $scope.result = "This is a Palindrome.";
+      $scope.result = $scope.newItem + " This is a Palindrome.";
     } else{
-      $scope.result = "This is not a Palindrome.";
+      $scope.result = $scope.newItem + " This is not a Palindrome.";
     }
   $scope.newItem = "";
-  
+
 }
 
 
